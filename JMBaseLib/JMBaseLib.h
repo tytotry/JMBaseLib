d98ffc6e365be5e2756ad18210314ba5d27ac808
JimmyLiang <656509491@qq.com>
2018-04-20 09:52:53 +0800

    first commit

---
+++ JMBaseLib/JMBaseLib.h
@@ -0,0 +1,19 @@
+//
+//  JMBaseLib.h
+//  JMBaseLib
+//
+//  Created by 梁建斌 on 2018/4/20.
+//  Copyright © 2018年 SunlandGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JMBaseLib.
+FOUNDATION_EXPORT double JMBaseLibVersionNumber;
+
+//! Project version string for JMBaseLib.
+FOUNDATION_EXPORT const unsigned char JMBaseLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JMBaseLib/PublicHeader.h>
+
+
---
+++ JMBaseLib/Lib/JMTest.swift
@@ -0,0 +1,15 @@
+//
+//  JMTest.swift
+//  JMBaseLib
+//
+//  Created by 梁建斌 on 2018/4/20.
+//  Copyright © 2018年 SunlandGroup. All rights reserved.
+//
+
+import UIKit
+
+public class JMTest: NSObject {
+    public func Log(_ log: String) {
+        print("JMTest Log");
+    }
+}
